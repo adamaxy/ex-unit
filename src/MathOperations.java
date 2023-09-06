96966f205a1a4402496ebe0038bfd2659362ad5a
kira <kira@MacBook-Air-di-kira.local>
2023-09-06 18:11:45 +0200

    ok

---
+++ src/MathOperations.java
@@ -0,0 +1,10 @@
+public class MathOperations {
+
+    public static int subtraction(int minuend, int subtrahend) {
+        return minuend - subtrahend;
+    }
+
+    public static int multiplication(int multiplier, int multiplicand) {
+        return multiplier * multiplicand;
+    }
+}
